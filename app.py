8f851a1de69794a1db80afa5e0ae563db33cf3aa
a-h-soikot <ahsoikot2022@gmail.com>
2025-05-01 12:31:11 +0600

    Initial Commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, redirect, url_for, render_template
+from livereload import Server
+
+app = Flask(__name__)
+
+@app.route("/")
+def func():
+    return render_template("paper.html")
+
+if __name__ == "__main__":
+    app.debug = True
+
+    # Use livereload server instead of app.run
+    server = Server(app.wsgi_app)
+    # Watch for changes in templates and static files
+    server.watch('templates/')
+    server.watch('static/')
+    # Start the livereload server
+    server.serve(port=5000)
