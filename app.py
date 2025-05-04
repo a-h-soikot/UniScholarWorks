704eed4788ca643948f42ffbb3e627b2a6fa53bd
a-h-soikot <ahsoikot2022@gmail.com>
2025-05-04 15:48:24 +0600

    Add registration page

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def func():
-    return render_template("paper.html")
+    return render_template("registration.html")
 
 if __name__ == "__main__":
     app.debug = True
